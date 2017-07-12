510fccc536ef27cea128a0c896e8880446162f8d
wahabalimalik <wahabalimalik@gmail.com>
2017-07-12 18:54:10 +0500

    [ADD] Adding new computational field in Sale/Sale/Token Money of Price Subtotal

--- sale_ext_token_money/models/sale_ext_token_money.py
+++ sale_ext_token_money/models/sale_ext_token_money.py
@@ -14,13 +14,13 @@ class SaleExt(models.Model):
 			if rec.product_id.product_tmpl_id.token_check == True:
 				token_money.create({
 					'name' : rec.product_id.product_tmpl_id.id,
-					'so_ref' : self.name,
+					'so_ref' : self.id,
 					'qunatity' : rec.product_uom_qty
 				})
 			else:
 				wo_token_money.create({
 					'name' : rec.product_id.product_tmpl_id.id,
-					'so_ref' : self.name,
+					'so_ref' : self.id,
 				})
 		return res
 
@@ -34,18 +34,21 @@ class TokenMoney(models.Model):
     _name = 'token.money'
 
     name = fields.Many2one('product.template',)
-    so_ref = fields.Char('Sale Order')
-    ttl_tok_money = fields.Float('Token Money',compute='_compute_total_money')
+    so_ref = fields.Many2one('sale.order','Sale Order')
+    ttl_tok_money = fields.Float('Token Money', compute='_compute_total_money')
+    price_subtotal = fields.Float('Price Subtotal', compute='_compute_total_money')
     qunatity = fields.Integer()
 
-    @api.depends('name')
+    @api.depends('name','so_ref')
     def _compute_total_money(self):
     	for rec in self:
 		    if rec.name:
 		        rec.ttl_tok_money = rec.name.token_money * rec.qunatity
+		    if rec.so_ref:
+		    	rec.price_subtotal = [rec.price_subtotal + x.price_subtotal for x in rec.so_ref.order_line if x.product_id.name == rec.name.name][0]
 
 class WithoutTokenMoney(models.Model):
     _name = 'wo.token.money'
 
     name = fields.Many2one('product.template')
-    so_ref = fields.Char('Sale Order')
+    so_ref = fields.Many2one('sale.order','Sale Order')
